66b705d94ffba2976af3d8b9584410850f1d00e4
Daniel Zárate <49570493+Danifado@users.noreply.github.com>
2021-05-16 13:16:49 -0500

    Codigo de R

---
+++ covid.R
@@ -0,0 +1,11 @@
+data$Fecha_de_Notificación <- as.Date(data$Fecha_de_Notificación, "%d/%m/%Y")
+
+data$Fecha_de_Inicio_de_Síntomas <- as.Date(data$Fecha_de_Inicio_de_Síntomas, "%d/%m/%Y")
+
+data$Fecha_de_Diagnóstico <- as.Date(data$Fecha_de_Diagnóstico, "%d/%m/%Y")
+
+data$Fecha_de_Recuperación <- as.Date(data$Fecha_de_Recuperación, "%d/%m/%Y")
+
+data$Fecha_de_Muerte <- as.Date(data$Fecha_de_Muerte, "%d/%m/%Y")
+
+
